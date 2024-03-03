c200aed400fc6269cac255172104fd3c420610ae
Tejraj Pawar <pawartejraj47@gmail.com>
2024-03-02 20:43:50 -0500

    Create 49_Group_Anagrams.py

---
+++ 49_Group_Anagrams.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # TC: O(n)  SC: O(n)
+        track = {}
+        for item in strs:
+            sortStr = "".join(sorted(item))
+            if sortStr in track:
+                track[sortStr].append(item)
+            else:
+                track[sortStr] = [item]
+        
+        return track.values()
