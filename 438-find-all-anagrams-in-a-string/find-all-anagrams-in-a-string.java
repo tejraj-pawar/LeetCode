c7d25a7afdb53641c821fcfa777b137e72f90aa3
Tejraj Pawar <pawartejraj47@gmail.com>
2024-07-07 13:19:23 -0400

    Time: 74 ms (26.60%) | Memory: 45.4 MB (27.41%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,6 +1,7 @@
 class Solution {
+    /* Brute-Force: Use sliding window approach and sort window_str and compare with p
     public List<Integer> findAnagrams(String s, String p) {
-
+        
         List<Integer> res = new ArrayList<>();
         int k = p.length();
 
@@ -8,9 +9,7 @@ class Solution {
 
         for(int i=0; i<s.length()-k+1; i++) {
             String temp = s.substring(i, i+k);
-            
             if(p.equals(sortString(temp))) res.add(i);
-
         }
         return res;
     }
@@ -20,4 +19,42 @@ class Solution {
         Arrays.sort(tempArray);
         return String.valueOf(tempArray);
     }
+    */
+
+    public List<Integer> findAnagrams(String s, String p) {
+        int sLen = s.length();
+        int pLen = p.length();
+
+        if(sLen < pLen) return new ArrayList<>();
+        
+
+        List<Integer> res = new ArrayList<>();
+
+        HashMap<Character, Integer> sMap = new HashMap<>();
+        HashMap<Character, Integer> pMap = new HashMap<>(); 
+
+        for(int i=0; i<pLen; i++) {
+            pMap.put(p.charAt(i), pMap.getOrDefault(p.charAt(i), 0) + 1);
+            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        if(sMap.equals(pMap)) res.add(0);
+
+        for(int i=pLen; i<sLen; i++) {
+            //Add new window element(front)
+            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
+            //Remove existing window element(rear)
+            char discard = s.charAt(i-pLen);
+            sMap.put(discard, sMap.get(discard) - 1);
+
+            // This is needed or else while comparing it will break
+            if(sMap.get(discard) == 0) sMap.remove(discard);
+
+            if(sMap.equals(pMap)) res.add(i - pLen + 1);
+        }
+
+        return res;
+
+    }
+
 }
