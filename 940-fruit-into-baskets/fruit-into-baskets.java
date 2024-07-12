da963972d2bf52df32994959dd6d6a749aab963c
Tejraj Pawar <pawartejraj47@gmail.com>
2024-07-12 12:11:10 -0400

    Time: 52 ms (36.40%) | Memory: 55 MB (56.03%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -2,20 +2,22 @@ class Solution {
     public int totalFruit(int[] fruits) {
         /*https://www.youtube.com/watch?v=s_zu2dOkq80
         logic: starting point matters, i.e. you have to find largest sub-array of two numbers.*/
-        int ans = 0;
-        Map<Integer, Integer> count = new HashMap<>();
+        int res = 0;
+        Map<Integer, Integer> track = new HashMap<>();
 
-        for (int l = 0, r = 0; r < fruits.length; r++) {
-            count.put(fruits[r], count.getOrDefault(fruits[r], 0) + 1);
-            while (count.size() > 2) {
-                count.put(fruits[l], count.get(fruits[l]) - 1);
-                count.remove(fruits[l], 0);
+        for(int l = 0, r = 0; r < fruits.length; r++) {
+            track.put(fruits[r], track.getOrDefault(fruits[r], 0) + 1);
+
+            while(track.size() > 2) {
+                track.put(fruits[l], track.get(fruits[l]) - 1);
+                track.remove(fruits[l], 0); // remove if fruits[l] has value zero.
                 l++;
             }
-            ans = Math.max(ans, r - l + 1);
-        }
 
-        return ans;
+            res = Math.max(res, r - l + 1);
+            
+        }
+        return res;
         
     }
 }
